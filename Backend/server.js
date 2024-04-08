f05c087095fb72926020b9f7b118e0474a4d716c
Prasanna V <you@example.com>
2024-04-08 14:17:17 +0530

    added a DB in mongoDB and connected it to server

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const API_KEY = process.env.API_KEY;
 
-let app = express();
+const app = express();
+const ConnnectDB = async () => {
+  try {
+    await mongoose.connect(API_KEY);
+    console.log('connected to DB');
+  } catch (error) {
+    console.log(error, 'error in connecting');
+  }
+};
+
 app.use(cors());
 
 app.listen(3553, async () => {
   console.log('Port listening to 3553 port');
+  ConnnectDB()
 });
