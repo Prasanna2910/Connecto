cdc6fabb1647cafee2215c4061acd4ea0776021e
Prasanna V <you@example.com>
2024-04-08 16:48:22 +0530

    added the db

--- Backend/server.js
+++ Backend/server.js
@@ -3,21 +3,22 @@ const cors = require('cors');
 const axios = require('axios');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
-const API_KEY = process.env.API_KEY;
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
-const connnectDB = async () => {
+app.use(cors());
+
+const connectDB = async () => {
   try {
-    await mongoose.connect(API_KEY);
+    await mongoose.connect(MONGO_URI);
     console.log('connected to DB');
   } catch (error) {
     console.log(error, 'error in connecting');
   }
 };
 
-app.use(cors());
 
 app.listen(3553, async () => {
   console.log('Port listening to 3553 port');
-  connnectDB()
+  await connectDB();
 });
