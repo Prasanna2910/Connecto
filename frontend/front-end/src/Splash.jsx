7320b03589790d8d5c364a21f77c5b3927979eb8
Prasanna V <you@example.com>
2024-04-15 17:45:46 +0530

    added a dignup page

--- frontend/front-end/src/Splash.jsx
+++ frontend/front-end/src/Splash.jsx
@@ -4,8 +4,8 @@ import EntityCard from './EntityCard';
 import Login from './Login';
 import './App.css';
 // import mainpagelogo from './assets/workerbackground.png';
-import connectoLogo from './assets/blacksplash.png';
-import workerimage from './assets/worker_image-removebg-preview.png';
+import connectoLogo from './assets/blacksplashA.png';
+// import workerimage from './assets/worker_image-removebg-preview.png';
 import { useNavigate } from 'react-router-dom';
 
 function Splash() {
@@ -26,27 +26,27 @@ function Splash() {
       });
   }, []);
   return (
-    <div className="bg-[#171717] h-full flex ">
+    <div className="bg-gray-400 h-full flex ">
       <div className="w-1/2 h-screen justify-center items-center ml-28 grid">
-        <img src={workerimage} alt="" className="w-1/5 absolute top-20 " />
+        {/* <img src={workerimage} alt="" className="w-1/5 absolute top-20 " /> */}
         <img
           src={connectoLogo}
           alt=""
-          className=" w-1/5 h-2/6  absolute top-72  "
+          className=" w-2/5 h-4/5  absolute top-9 left-5  "
         />
       </div>
-      <div className="bg-[#DA0037] w-3/4">
-        <div className="mt-24 w-1/2 ml-32 text-center">
-          <h1 className="text-6xl font-semibold text-white">Welcome!!</h1>
+      <div className="bg-white w-3/4">
+        <div className="mt-24 w-3/4 ml-20 text-center">
+          <p className="text-black text-3xl font-bold">Work with Us</p>
           <br />
-          <p className='text-white text-xl'>A platform for everyone to find work</p>
-          <p className="text-white text-l">Work with Us</p>
-          <p className="text-white text-l">Work from Us</p>
-          <p className="text-white text-xl">Let us Work Together</p>
+          <p className="text-black text-3xl font-bold">Work from Us</p>
+          <br />
+          <p className="text-black text-3xl font-bold">Let us Work Together</p>
+          <br />
         </div>
-        <div className="mt-8 ml-[226px]">
+        <div className="mt-7 ml-[248px]">
           <button
-            className="text-white hover:text-green-500 backdrop-blur-lg bg-gradient-to-tr bg-black via-[rgba(121,121,121,0.16)]  rounded-md py-1 px-4 shadow hover:shadow-white duration-700"
+            className="text-red-400 border-red-400 border hover:text-red-400 backdrop-blur-lg bg-gradient-to-tr bg-white via-[rgba(121,121,121,0.16)]  rounded-md py-1 px-4 shadow hover:shadow-red-400 duration-700"
             onClick={() => {
               navigate(`/Login`);
             }}
