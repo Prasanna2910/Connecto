61fa8bc8f1d6f48d040fd999ea95aa10bd559f32
Prasanna V <you@example.com>
2024-04-16 18:35:09 +0530

    added joi validation to signup page

--- frontend/front-end/src/Signup.jsx
+++ frontend/front-end/src/Signup.jsx
@@ -1,55 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './Login';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    confirmemail: '',
+    password: '',
+    confirmpassword: '',
+    gender: 'male',
+  });
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:3553/signup', formData);
+      console.log(res);
+      setFormData({
+        name: '',
+        email: '',
+        confirmemail: '',
+        password: '',
+        confirmpassword: '',
+        gender: 'male',
+      });
+    } catch (err) {
+      console.log('error', err.response.data);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center h-3/4 ">
       <div className="w-full max-w-md rounded-lg shadow-md p-6">
-        <h2 className="text-2xl font-bold text-red-500 mb-4">Sign Up</h2>
-        <form className="flex flex-col">
+        <h2 className="text-2xl font-bold text-blue-500 mb-4">Sign Up</h2>
+        <form className="flex flex-col" onSubmit={handleSubmit}>
           <input
             placeholder="Name"
+            name="name"
+            value={formData.name}
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             type="text"
+            onChange={handleChange}
           />
 
           <input
             placeholder="Email"
+            name="email"
+            value={formData.email}
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             type="email"
+            onChange={handleChange}
           />
           <input
             placeholder="Confirm Email"
+            name="confirmemail"
+            value={formData.confirmEmail}
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             type="email"
+            onChange={handleChange}
           />
           <input
             placeholder="Password"
+            name="password"
+            value={formData.password}
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             type="password"
+            onChange={handleChange}
           />
           <input
             placeholder="Confirm Password"
+            name="confirmpassword"
+            value={formData.confirmpassword}
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             type="password"
+            onChange={handleChange}
           />
 
           <select
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             id="gender"
+            name="gender"
+            value={formData.gender}
+            onChange={handleChange}
           >
             <option value="male">Male</option>
             <option value="female">Female</option>
             <option value="other">Other</option>
           </select>
-
-          <input
-            className="bg-gray-100 text-gray-900 border-0 rounded-md p-2"
-            id="age"
-            type="date"
-          />
           <p className="text-gray-900 mt-4">
             {' '}
             Already have an account?{' '}
@@ -65,6 +109,7 @@ function Signup() {
           <button
             className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 transition ease-in-out duration-150"
             type="submit"
+            // onSubmit={handleSubmit}
           >
             Sign Up
           </button>
