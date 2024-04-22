19a4a945596934eec22e131d7f22ff3ea0546f1c
Prasanna V <you@example.com>
2024-04-22 19:27:25 +0530

    added a main page which displays data

---
+++ frontend/front-end/src/mainpg.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import EntityCard from './EntityCard';
+
+function mainpage() {
+  const [entities, setEntities] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://connecto-dupe.onrender.com/main')
+      .then((res) => {
+        console.log('get in frontend is working');
+        if (res.data.length > 0) {
+          setEntities(res.data);
+        }
+      })
+      .catch((error) => {
+        console.log('error in getting', error);
+      });
+  }, []);
+  return (
+    <div className="grid grid-cols-2 gap-8">
+      {entities.map((entity, index) => (
+        <div
+          key={index}
+          className="w-8/12 ml-20 bg-white rounded-xl grid items-center"
+        >
+          <EntityCard key={index} entity={entity} />
+        </div>
+      ))}
+    </div>
+  );
+}
+export default mainpage;
