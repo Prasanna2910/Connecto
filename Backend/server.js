3a6b65d51f9abcab18f3c07c4977a03a14694528
Prasanna V <you@example.com>
2024-04-24 16:05:08 +0530

    connected the login and signup and hashed the password

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv').config();
 const joi = require('joi');
 const bodyParser = require('body-parser');
 const Joi = require('joi');
+const bcrypt = require('bcryptjs');
 const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
@@ -31,22 +32,23 @@ const userJoiSchema = Joi.object({
     .required(),
   gender: Joi.string().required(),
 });
+
 const schema = new mongoose.Schema({
   Name: String,
   Work: String,
   PhoneNumber: Number,
 });
-const NewModelForSchema = new mongoose.Schema({
+const userModelSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
   gender: String,
 });
 
-const ModelForJoi = mongoose.model('AuthDB', NewModelForSchema);
+const User = mongoose.model('AuthDB', userModelSchema);
 const Model = mongoose.model('db', schema);
 
-app.get('/', (req, res) => {
+app.get('/main', (req, res) => {
   Model.find({})
     .then((data) => {
       res.json(data);
@@ -66,22 +68,48 @@ app.post('/main', async (req, res) => {
   }
 });
 
-app.post('/', async (req, res) => {
-  const { error, value } = userJoiSchema.validate(req.body);
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
+app.post('/signup', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    res.send({ message: 'get out' });
+  } else {
+    const { error, value } = userJoiSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    if (req.body.email !== req.body.confirmemail) {
+      return res.status(400).json({ message: 'Emails do not match' });
+    }
+    if (req.body.password !== req.body.confirmpassword) {
+      return res.status(400).json({ message: 'Passwords do not match' });
+    }
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(value.password, salt);
+      const newUser = await User.create({ ...value, password: hashedPassword });
+      res.send({ message: 'ok' });
+    } catch (error) {
+      console.log('error', error);
+    }
   }
-  if (req.body.email !== req.body.confirmemail) {
-    return res.status(400).json({ message: 'Emails do not match' });
-  }
-  if (req.body.password !== req.body.confirmpassword) {
-    return res.status(400).json({ message: 'Passwords do not match' });
-  }
+});
+
+app.post('/login', async (req, res) => {
   try {
-    const newUser = await ModelForJoi.create(value);
-    res.send(newUser);
+    const user = await User.findOne({ email: req.body.email });
+    // console.log(await bcrypt.compare(req.body.password, user.password));
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // console.log(user.password, hashedPassword);
+    if (user && bcrypt.compare(req.body.password,user.password)) {
+      // alert('Login successful');
+      res.json({ message: 'Login successful' });
+    } else {
+      res.send({ message: 'Invalid credentials or user not existed' });
+    }
   } catch (error) {
-    console.log('error', error);
+    console.log(error);
+    res.send('Error in login');
   }
 });
 
--- frontend/front-end/src/mainpg.jsx
+++ frontend/front-end/src/mainpg.jsx
@@ -3,14 +3,14 @@ import axios from 'axios';
 import EntityCard from './EntityCard';
 import { useNavigate } from 'react-router-dom';
 
-function mainpage() {
+function mainpg() {
   const [entities, setEntities] = useState([]);
   const navigate = useNavigate();
   useEffect(() => {
     axios
-      .get('https://connecto-dupe.onrender.com/main')
+      .get('https://connecto-dupe.onrender.com/')
       .then((res) => {
-        console.log('get in frontend is working');
+        console.log('Fetch successful');
         if (res.data.length > 0) {
           setEntities(res.data);
         }
@@ -19,11 +19,14 @@ function mainpage() {
         console.log('error in getting', error);
       });
   }, []);
+  if (entities.lennght === 0){
+    return <div>Loading...</div>
+  }
   return (
     <div className="grid grid-cols-2 gap-8">
       {entities.map((entity, index) => (
         <div
-          key={index}
+          key={entity.id || index}
           className="w-8/12 ml-20 bg-white rounded-xl grid items-center"
         >
           <EntityCard key={index} entity={entity} />
@@ -32,4 +35,4 @@ function mainpage() {
     </div>
   );
 }
-export default mainpage;
+export default mainpg;
