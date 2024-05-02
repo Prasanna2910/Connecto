6f52dc92caf893923b3847006abd4e1104f4f3b8
Prasanna V <you@example.com>
2024-05-02 09:17:57 +0530

    added a apply button in two places

--- frontend/front-end/src/Splash.jsx
+++ frontend/front-end/src/Splash.jsx
@@ -46,7 +46,7 @@ function Splash() {
             </p>
           </div>
           <br />
-          <div>
+          <div className='flex justify-around'>
             <button
               className="w-52 bg-black p-2 h-[50px] my-3 flex items-center justify-center rounded cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-sky-500 before:to-sky-500 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded hover:before:left-0 text-[#fff] font-medium"
               onClick={() => {
@@ -54,6 +54,11 @@ function Splash() {
               }}
             >
               Click Here to Proceed
+            </button>
+            <button
+              className="w-52 bg-black p-2 h-[50px] my-3 flex items-center justify-center rounded cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-sky-500 before:to-sky-500 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded hover:before:left-0 text-[#fff] font-medium"
+            >
+              Apply yourself
             </button>
           </div>
         </div>
@@ -68,48 +73,3 @@ function Splash() {
 }
 export default Splash;
 
-{
-  /* <div className="grid grid-cols-2 gap-8 ">
-  {entities.map((entity, index) => (
-    <div
-    key={index}
-    className=" w-8/12 ml-20 bg-white  rounded-xl grid items-center"
-    >
-    <div>
-        <EntityCard key={index} entity={entity} />
-      </div>
-    </div>
-  ))}
-</div> */
-}
-
-// <div className="bg-gray-400 h-full flex ">
-//   <div className="w-1/2 h-screen justify-center items-center ml-28 grid">
-//     {/* <img src={workerimage} alt="" className="w-1/5 absolute top-20 " /> */}
-//     <img
-//       src={connectoLogo}
-//       alt=""
-//       className=" w-2/5 h-4/5  absolute top-9 left-5  "
-//     />
-//   </div>
-//   <div className="bg-white w-3/4">
-//     <div className="mt-24 w-3/4 ml-20 text-center">
-//       <p className="text-black text-3xl font-bold">Work with Us</p>
-//       <br />
-//       <p className="text-black text-3xl font-bold">Work from Us</p>
-//       <br />
-//       <p className="text-black text-3xl font-bold">Let us Work Together</p>
-//       <br />
-//     </div>
-//     <div className="flex justify-center items-center">
-//       <button
-//         className="text-red-400 border-red-400 border hover:text-red-400 backdrop-blur-lg bg-gradient-to-tr bg-white via-[rgba(121,121,121,0.16)]  rounded-md py-1 px-4 shadow hover:shadow-red-400 duration-700"
-//         onClick={() => {
-//           navigate(`/Login`);
-//         }}
-//       >
-//         Get started
-//       </button>
-//     </div>
-//   </div>
-// </div>
